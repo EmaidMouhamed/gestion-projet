e0f8d79bcddb9c74385d627e800ac18b1210506e
EmaidMouhamed <diamemouhamed2000@gmail.com>
2024-07-26 12:01:48 +0000

    ajout des migration de projet & "string" to "text" pour role

--- database/migrations/2024_07_25_102207_create_roles_table.php
+++ database/migrations/2024_07_25_102207_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('nom', 50);
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->boolean('etat')->default(true);
             $table->timestamps();
         });
